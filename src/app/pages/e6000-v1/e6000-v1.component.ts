1e5e11581a5d67c2b3741d54e0d9ea43ff0d0207
Daniel <dny.velasquez@gmail.com>
2022-02-14 16:39:25 -0500

    E6000v1 start

---
+++ src/app/pages/e6000-v1/e6000-v1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e6000-v1',
+  templateUrl: './e6000-v1.component.html',
+  styleUrls: ['../../shared/styles/generalStyles.scss']
+})
+export class E6000V1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
