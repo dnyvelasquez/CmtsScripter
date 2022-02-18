f86db8d8498dc6bfc72119882d4e6f4da0c922c9
Daniel <dny.velasquez@gmail.com>
2022-02-18 18:48:39 -0500

    c100g docsis up

--- src/app/pages/c100-g/c100-g.component.ts
+++ src/app/pages/c100-g/c100-g.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-c100-g',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class C100GComponent implements OnInit {
 
-  constructor() { }
+  mac: string = '';
+  desc: string = '';
+  panelOpenStateAtt = false
+  quot: string = '"';
+  upStreams: number[] = [8];
+  ifUpstreams: number[] = [13, 12, 11, 10, 9];
+  sifUpstreams: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  sltUpstreams: number[] = [0, 1, 2, 3];
+
+  constructor(private sharedSvc: SharedService) { }
 
   ngOnInit(): void {
   }
+
+  inputCommand(command: string, pos: number){
+    this.sharedSvc.addCommand(command, pos);
+  }
+
+  addUpStream(up: number){
+    this.upStreams.push(up);
+  }
+  
+  subUpStream(){
+    this.upStreams.pop();
+  }
+
 
 }
