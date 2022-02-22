c61ef8b46f737189cdc74540db47a00086aa2932
Daniel <dny.velasquez@gmail.com>
2022-02-21 19:56:34 -0500

    downstreams channels

--- src/app/pages/c100-g/c100-g.component.ts
+++ src/app/pages/c100-g/c100-g.component.ts
@@ -11,6 +11,8 @@ export class C100GComponent implements OnInit {
   mac: string = '';
   desc: string = '';
   panelOpenStateAtt = false
+  panelOpenStateUs = false
+  panelOpenStateDs = false
   quot: string = '"';
   ifUpstreams: number[] = [13, 12, 11, 10, 9];
   sifUpstreams: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
@@ -20,7 +22,14 @@ export class C100GComponent implements OnInit {
   sifEndUp!: number;
   upstreamsA: number[] = [1, 2, 3, 4];
   upstreamsB: number[] = [5, 6, 7, 8];
-
+  ifDownstreams: number[] = [0, 1, 2, 3, 4];
+  sifDownstreams: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
+  chDownstreams: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                            24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
+  chDownEnd!: number;
+  interfaceDown!: number;
+  sifDown!: number;
+  downstreams: number[] = [];
 
   constructor(private sharedSvc: SharedService) { }
 
@@ -43,5 +52,9 @@ export class C100GComponent implements OnInit {
     this.upstreamsB.push(value);
   }
 
-
+  makeDownstreams(){
+    for(let i = 0; i <= this.chDownEnd; i++){
+      this.downstreams[i] = i + 1;
+    }
+  }
 }
