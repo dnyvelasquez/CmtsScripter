a954b131d0565e0cfb6377a7f134536fe2d5793c
Daniel <dny.velasquez@gmail.com>
2022-03-04 17:01:15 -0500

    e6000 int fiber function corrected

--- src/app/pages/e6000/fiber-node/fiber-node.component.ts
+++ src/app/pages/e6000/fiber-node/fiber-node.component.ts
@@ -15,15 +15,106 @@ export class FiberNodeComponent implements OnInit {
   @Input() pos!: number;
   @Input() sec: string = '';
   panelOpenState = false
-  quot: string = '"';
 
   constructor(private sharedSvc: SharedService) { }
 
   ngOnInit(): void {
   }
 
-  inputCommand(command: string, pos: number){
-    this.sharedSvc.addCommand(command, pos);
+  addFiberCommand(update: boolean, no: boolean){
+    let pos = this.pos;
+    let command = 'cable fiber-node';
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(`\n ${command} "${this.mac}${this.sec}"`, pos);
+      }else{
+        this.sharedSvc.addCommand(`\n no ${command} "${this.mac}${this.sec}"`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addInitCommand(update: boolean, no: boolean){
+    let pos = this.pos + 1;
+    let command = 'init';
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` ${command}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no ${command}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addDescCommand(update: boolean, text: string, no: boolean){
+    let pos = this.pos + 2;
+    let command = 'description';
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` ${command} "${text}"`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no ${command}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addDownCommand(update: boolean, no: boolean){
+    let pos = this.pos + 3;
+    let command = 'cable-downstream';
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` ${command} ${this.downstream}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no ${command} ${this.downstream}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addUpCommand(update: boolean, no: boolean){
+    let pos = this.pos + 4;
+    let command = 'cable-upstream';
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` ${command} ${this.upstream}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no ${command} ${this.upstream}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addPolicyCommand(update: boolean, text: string, no: boolean){
+    let pos = this.pos + 5;
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` cable-mac ${this.mac} load-balance policy ${text}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no cable-mac ${this.mac} load-balance policy ${text}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
+
+  addTechCommand(update: boolean, text: string, no: boolean){
+    let pos = this.pos + 5;
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` cable-mac ${this.mac} load-balance init-technique ${text}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no cable-mac ${this.mac} load-balance init-technique ${text}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
   }
 
 }
