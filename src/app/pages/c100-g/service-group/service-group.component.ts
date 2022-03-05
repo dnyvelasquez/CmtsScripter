bd50fd805f418a92c47bfe9e341ea2962a62e368
Daniel <dny.velasquez@gmail.com>
2022-03-04 19:25:04 -0500

    c100 int service + function corrected

--- src/app/pages/c100-g/service-group/service-group.component.ts
+++ src/app/pages/c100-g/service-group/service-group.component.ts
@@ -24,60 +24,52 @@ export class ServiceGroupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addCommand(id: string, update: boolean, no: boolean, text: string){
-    //service group
-    if(id === 'sgUpdate'){
-      if(update){
-        this.sharedSvc.addCommand(`\nservice group ${this.mac}${this.sec}`, this.pos);
-      }else{
-        this.sharedSvc.addCommand(``, this.pos);
-      }
-    }
-    //description
-    if(id === 'descUpdate'){
-      let pos = this.pos + 1;
-      let command = `description`
-      if(update){
-        if(!no){
-          this.sharedSvc.addCommand(` ${command} "${text}"`, pos);
-        }else{
-          this.sharedSvc.addCommand(` no ${command}`, pos);
-        }
-      }else{
-        this.sharedSvc.addCommand(``, pos);
-      }
+  addServiceCommand(update: boolean){
+    if(update){
+      this.sharedSvc.addCommand(`\nservice group ${this.mac}${this.sec}`, this.pos);
+    }else{
+      this.sharedSvc.addCommand(``, this.pos);
     }
   }
 
-    addServCommand(id: string, update: boolean, no: boolean, ch: number){
-      //qams
-      if(id === 'qamUpdate'){
-        let pos = this.pos + 2 + ch;
-        if(update){
-          if(!no){
-            this.sharedSvc.addCommand(` qam ${this.ifDown}/${this.sifDown}/${ch}`, pos);
-          }else{
-            this.sharedSvc.addCommand(` no qam ${this.ifDown}/${this.sifDown}/${ch}`, pos);
-          }
-        }else{
-          this.sharedSvc.addCommand(``, pos);
-        }
+  addDescCommand(update: boolean, no: boolean, text: string){
+    let pos = this.pos + 1;
+    let command = `description`
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` ${command} "${text}"`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no ${command}`, pos);
       }
-      //ups
-      if(id === 'upUpdate'){
-        let pos = this.pos + 90 + ch;
-        if(update){
-          if(!no){
-            this.sharedSvc.addCommand(` upstream ${this.ifUp}/${this.sifUp}.${ch}`, pos);
-          }else{
-            this.sharedSvc.addCommand(` no upstream ${this.ifUp}/${this.sifUp}.${ch}`, pos);
-          }
-        }else{
-          this.sharedSvc.addCommand(``, pos);
-        }
-      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
     }
+  }
 
+  addQamCommand(update: boolean, no: boolean, ch: number){
+    let pos = this.pos + 2 + ch;
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` qam ${this.ifDown}/${this.sifDown}/${ch}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no qam ${this.ifDown}/${this.sifDown}/${ch}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
 
+  addUpCommand(update: boolean, no: boolean, ch: number){
+    let pos = this.pos + 90 + ch;
+    if(update){
+      if(!no){
+        this.sharedSvc.addCommand(` upstream ${this.ifUp}/${this.sifUp}.${ch}`, pos);
+      }else{
+        this.sharedSvc.addCommand(` no upstream ${this.ifUp}/${this.sifUp}.${ch}`, pos);
+      }
+    }else{
+      this.sharedSvc.addCommand(``, pos);
+    }
+  }
 
 }
